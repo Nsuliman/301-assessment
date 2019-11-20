1abf8cd04f3075fb2786805124d9445ff2dd1910
Nsuliman <nsuliman88@gmail.com>
2019-11-20 17:59:27 +0200

    301 Assessment completed

--- README.md
+++ README.md
@@ -36,5 +36,39 @@ for (let i = 0;i<arrayNames.lenght;i++)
 
 
 
+# Application 
+
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3000;
+const server = express();
+server.use(cors());
+
+// First Route 
+server.get('/',(Request,Response) =>
+{
+Response.status(200).send('Home Page');
+});
+
+// Second Route 
+const object = { Name :'Nawal', age : 31};
+
+server.get('/data',(Request,Response) =>
+{
+Response.status(200).json(object);
+});
+
+// Third Route 
+server.get('/error',(Request,Response) =>
+{
+Response.status(404).send('Not Found');
+});
+
+
+server.listen(PORT, () => console.log('Listens on port ' , PORT));
+
 
 
--- app.js
+++ app.js
@@ -18,3 +18,40 @@
 // }
 
 // } // End of uppercase function 
+
+
+/************************************* Application Questions **********************************************/ 
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const PORT = process.env.PORT || 3000;
+const server = express();
+server.use(cors());
+
+// First Route 
+server.get('/',(Request,Response) =>
+{
+Response.status(200).send('Home Page');
+});
+
+// Second Route 
+const object = { Name :'Nawal', age : 31};
+
+server.get('/data',(Request,Response) =>
+{
+Response.status(200).json(object);
+});
+
+// Third Route 
+server.get('/error',(Request,Response) =>
+{
+Response.status(404).send('Not Found');
+});
+
+
+server.listen(PORT, () => console.log('Listens on port ' , PORT));
+
+
+
